27f87bffc0bfe5a8d6c8dabe8a635dd3d6366436
nbollis <nbollis@comcast.net>
2024-01-04 15:14:51 -0600

    Extract Metadata from sdrf

---
+++ MetaPAL/Resources/MetaData/SDRF/SdrfToMetaData.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MetaPAL.Resources.MetaData.SDRF
+{
+    public static class SdrfToMetaData
+    {
+        private static string _withinBraces = @"\[(.*?)\]";
+        public static Dictionary<string, List<string>> ParseMetaDataFile(string sdrfPath)
+        {
+            List<(string, List<string>)> headerToValues = new List<(string, List<string>)>();
+
+            // parse header and add all columns to dictionary then find distinct values
+            using (var streamReader = new StreamReader(File.OpenRead(sdrfPath)))
+            {
+                var header = streamReader.ReadLine();
+                var headerColumns = header.Split('\t');
+                foreach (var headerColumn in headerColumns)
+                {
+                    if (headerColumn.Contains("[") && headerColumn.Contains("]"))
+                    {
+                        var match = Regex.Match(headerColumn, _withinBraces);
+                        if (match.Success)
+                            headerToValues.Add((match.Groups[1].Value, new List<string>()));
+                    }
+                    else
+                        headerToValues.Add((headerColumn, new List<string>()));
+                }
+
+                while (!streamReader.EndOfStream)
+                {
+                    var line = streamReader.ReadLine();
+                    var values = line.Split('\t');
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        headerToValues[i].Item2.Add(values[i]);
+                    }
+                }
+            }
+            // collapse repeated headers and remove duplicates from values
+            return headerToValues.GroupBy(x => x.Item1)
+                .ToDictionary(x => x.Key, 
+                    x => x.SelectMany(y => y.Item2)
+                                                        .Distinct()
+                                                        .ToList());
+        }
+
+        public static IEnumerable<Models.MetaData> GetMetaData(string sdrfPath)
+        {
+            foreach (var (header, values) in ParseMetaDataFile(sdrfPath))
+                foreach (var value in values)
+                    yield return new Models.MetaData() { Name = header, Value = value };
+        }
+    }
+}
---
+++ Test/TestMetaDataExtraction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetaPAL.Resources.MetaData.SDRF;
+
+namespace Test
+{
+    internal class TestMetaDataExtraction
+    {
+        private string _sdrfPath_2137 = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "MetaData",
+            "PXD002137.sdrf.tsv");
+
+        private string[] _expectedHeaders_2137 = new[]
+        {
+            "source name", "organism", "organism part", "cell type", "cell line", "developmental stage", "disease",
+            "ancestry category", "phenotype", "sex", "age", "biological replicate", "individual", "Material Type",
+            "assay name", "technology type", "data file", "file uri", "technical replicate", "fraction identifier",
+            "label", "instrument", "cleavage agent details", "modification parameters", "dissociation method",
+            "collision energy", "precursor mass tolerance", "fragment mass tolerance"
+        };
+
+        [Test]
+        public void TestHeaderParsing()
+        {
+            var parsedHeader = SdrfToMetaData.ParseMetaDataFile(_sdrfPath_2137);
+            CollectionAssert.AreEqual(_expectedHeaders_2137, parsedHeader.Keys);
+            Assert.That(parsedHeader["data file"].Count, Is.EqualTo(192));
+            Assert.That(parsedHeader["source name"].Count, Is.EqualTo(32));
+            Assert.That(parsedHeader["organism"].Count, Is.EqualTo(1));
+            Assert.That(parsedHeader["sex"].Count, Is.EqualTo(2));
+            Assert.That(parsedHeader["sex"].Contains("male"));
+            Assert.That(parsedHeader["sex"].Contains("Female"));
+        }
+
+        [Test]
+        public void TestGetMetaData()
+        {
+            var metaData = SdrfToMetaData.GetMetaData(_sdrfPath_2137).ToList();
+            Assert.That(metaData.Count(p => p.Name == "data file"), Is.EqualTo(192));
+            Assert.That(metaData.Count(p => p.Name == "source name"), Is.EqualTo(32));
+            Assert.That(metaData.Count(p => p.Name == "organism"), Is.EqualTo(1));
+        }
+    }
+}
